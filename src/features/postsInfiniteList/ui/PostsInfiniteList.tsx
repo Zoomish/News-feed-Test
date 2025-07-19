48d393adbb4503937bb4599e6aec1d8db2249bc4
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-07-19 16:24:10 +0300

    `feat: updated PostsInfiniteList component with new props, state and functionality`

--- src/features/postsInfiniteList/ui/PostsInfiniteList.tsx
+++ src/features/postsInfiniteList/ui/PostsInfiniteList.tsx
@@ -1,51 +1,56 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { useGetPostsQuery } from "@/entities/post/model/postApi";
-import { Post } from "@/entities/post/types";
 import { PostCard } from "@/entities/post/ui/PostCard";
 import { useInfinityScroll } from "@/shared/hooks/useInfinityScroll";
-import { Spin } from "antd";
-import { useState } from "react";
+import { Post } from "@/entities/post/types";
 
-export const PostsInfiniteList = () => {
+type Props = {
+    initialPosts: Post[];
+    total: number;
+};
+
+export const PostsInfiniteList = ({ initialPosts, total }: Props) => {
     const limit = 10;
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
+    const [posts, setPosts] = useState<Post[]>(initialPosts);
+
     const skip = page * limit;
 
-    const { data, isFetching } = useGetPostsQuery({ limit, skip });
+    const { data, isFetching } = useGetPostsQuery(
+        { limit, skip },
+        { skip: page === 0 },
+    );
 
-    const [allPosts, setAllPosts] = useState<Post[]>([]);
-    const [hasMore, setHasMore] = useState(true);
-
-    const handleLoadMore = () => {
-        if (!isFetching) {
-            setPage((prev) => prev + 1);
+    useEffect(() => {
+        if (data?.posts) {
+            setPosts((prev) => [...prev, ...data.posts]);
         }
-    };
+    }, [data]);
+
+    const hasMore = posts.length < total;
 
     const lastItemRef = useInfinityScroll({
         hasMore,
         isLoading: isFetching,
-        onLoadMore: handleLoadMore,
+        onLoadMore: () => setPage((prev) => prev + 1),
     });
 
-    if (data?.posts && allPosts.length < data.total) {
-        setAllPosts((prev) => [...prev, ...data.posts]);
-        setHasMore(allPosts.length + data.posts.length < data.total);
-    }
-
     return (
-        <div>
-            {allPosts.map((post, i) => (
+        <div className="mt-6">
+            {posts.map((post, idx) => (
                 <div
-                    ref={i === allPosts.length - 1 ? lastItemRef : null}
+                    ref={idx === posts.length - 1 ? lastItemRef : null}
                     key={post.id}
                 >
                     <PostCard post={post} />
                 </div>
             ))}
             {isFetching && (
-                <Spin style={{ display: "block", margin: "20px auto" }} />
+                <div className="text-center py-4 text-gray-500">
+                    Загрузка...
+                </div>
             )}
         </div>
     );
