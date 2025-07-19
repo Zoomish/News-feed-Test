8c4da56d62652ee9989807a3a94b3966b64803fd
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-07-19 16:23:57 +0300

    `Added @tailwindcss/line-clamp plugin and dependency`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import lineClamp from "@tailwindcss/line-clamp";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -9,6 +10,6 @@ const config: Config = {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [lineClamp],
 };
 export default config;
