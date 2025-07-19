bf7d69960b11157748815ef1109e4cf30584f1a7
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-07-19 16:24:36 +0300

    `Replace PostCard with PostsInfiniteList in Home component`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { PostsResponse } from "@/entities/post/types";
-import { PostCard } from "@/entities/post/ui/PostCard";
+import { PostsInfiniteList } from "@/features/postsInfiniteList/ui/PostsInfiniteList";
 
 const LIMIT = 10;
 
@@ -19,9 +19,7 @@ export default async function Home() {
     return (
         <main className="max-w-2xl mx-auto px-4 py-8">
             <h1 className="text-3xl font-bold mb-6">Новости</h1>
-            {data.posts.map((post) => (
-                <PostCard key={post.id} post={post} />
-            ))}
+            <PostsInfiniteList initialPosts={data.posts} total={data.total} />
         </main>
     );
 }
